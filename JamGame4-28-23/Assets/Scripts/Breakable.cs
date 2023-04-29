ead8229bd9ac0f88777d40832742186fb02c3fa8
mug806 <mug806@gmail.com>
2023-04-29 14:11:07 +0100

    L 6 & 7

--- JamGame4-28-23/Assets/Scripts/Breakable.cs
+++ JamGame4-28-23/Assets/Scripts/Breakable.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    public float minVelocity = 2;
     public int health = 1; // Amount of times to hit object before it breaks
     // Rewrite velocity check code, doesn't work
 
+    ParticleSystem particles;
+
+    private void Start() {
+        particles = GetComponentInChildren<ParticleSystem>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag != "Hammer") // If velocity is not met
+        if (collision.gameObject.tag != "Hammer" || collision.relativeVelocity.sqrMagnitude < (minVelocity*minVelocity))
         {
             return;
         }
 
         health--;
-        if (health == 0)
-        {
+        particles.Emit(10);
+
+        if (health == 0) {
+            particles.Emit(50);
+            particles.transform.parent = null;
+            Destroy(particles.gameObject, 5);
             Destroy(this.gameObject);
         }
     }
